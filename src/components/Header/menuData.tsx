d56aa43bbb8659d8b0956520c2fe12f380452e5e
Andre Lobo <loboandre@hotmail.com>
2024-03-04 16:54:34 -0400

    initial commits

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,24 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Início",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Sobre nós",
+    path: "/about",
+    newTab: false,
+  },
+    {
+    id: 3,
+    title: "Contato",
+    path: "/contact",
+    newTab: false,
+  },
+  
+];
+export default menuData;
