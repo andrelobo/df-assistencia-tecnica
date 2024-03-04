b76b3f0e5df562ec158532a24184cc291a190615
Andre Lobo <loboandre@hotmail.com>
2024-03-04 17:32:31 -0400

    feat: new logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -49,7 +49,7 @@ const Header = () => {
       >
         <div className="container">
           <div className="relative -mx-4 flex items-center justify-between">
-            <div className="w-60 max-w-full px-4 xl:mr-12">
+            <div className="w-80 max-w-full px-4 xl:mr-12">
               <Link
                 href="/"
                 className={`header-logo block w-full ${
@@ -59,15 +59,15 @@ const Header = () => {
                 <Image
                   src="/images/logo/logo.png"
                   alt="logo"
-                  width={400}
-                  height={53}
+                  width={478}
+                  height={56}
                   className="w-full dark:hidden"
                 />
                 <Image
                   src="/images/logo/logo.png"
                   alt="logo"
-                  width={400}
-                  height={53}
+                  width={478}
+                  height={56}
                   className="hidden w-full dark:block"
                 />
               </Link>
